616da7e47725d59bb5e2b6ffcdf4669cd96f5452
SLiemann <sebastian.liemann@tu-dortmund.de>
2021-04-28 16:00:05 +0200

    Update hybrid example

--- src/run/playground.jl
+++ src/run/playground.jl
@@ -1,5 +1,6 @@
 using DifferentialEquations
 using Plots
+using DataFrames
 
 function Subsystem1(du,u,p,t)
     du[1] = -u[1] + p[1]*u[2]
@@ -32,31 +33,38 @@ end
 cb1 = DiscreteCallback(condition1,affect1)
 cb2 = DiscreteCallback(condition2,affect2)
 
-u0 = [0.7;0.7]
-#u0 = [0.7;0.0]
-tspan = (0.0,1.0)
-p = [10.0,-100.0]
-prob = ODEProblem(Subsystem1,u0,tspan,p)
-sol = solve(prob,Rodas4(),dtmax=1e-3, callback = CallbackSet(cb1,cb2))
-plot(sol,vars =(1,2))
+plot()
+fig2 = []
+u0 = -[0.7;0.7]
+for i in 1:7
+    tspan = (0.0,1.0)
+    p = [10.0,-100.0]
+    prob = ODEProblem(Subsystem1,u0,tspan,p)
+    sol = solve(prob,Rodas4(),dtmax=1e-4, callback = CallbackSet(cb1,cb2))
 
-fig = plot(sol,vars=(1,2),size = (400, 300), legend = false,
-            framestyle = :box,
-            linewidth = 1.5,
-            xlabel = "\$x_1\$",
-            labelfontsize = 15,
-            #xlims = (-2.5,2.5),
-            xlims = (-0.7,0.7),
-            xtickfont = font(8, "Akkurat"),
-            ylabel = "\$x_2\$",
-            #ylims = (-2.5,2.5),
-            ylims = (-0.7,0.7),
-            #linecolor = RGB(146/255,208/255,80/255),
-            #linecolor = RGB(244/255,177/255,131/255),
-            ytickfont = font(8, "Akkurat"),
-            grid = true,
-            gridalpha = 0.5,
-            gridstyle = :dash
-            )
+    fig2 = plot!(sol,vars=(1,2),size = (400, 300), legend = false,
+                framestyle = :box,
+                linewidth = 1.5,
+                xlabel = "\$x_1\$",
+                labelfontsize = 15,
+                #xlims = (-2.5,2.5),
+                xlims = (-1,1),
+                xtickfont = font(8, "Akkurat"),
+                ylabel = "\$x_2\$",
+                #ylims = (-2.5,2.5),
+                ylims = (-1,1),
+                #linecolor = RGB(146/255,208/255,80/255),
+                #linecolor = RGB(244/255,177/255,131/255),
+                linecolor = RGB(0/255,0/255,255/255),
+                ytickfont = font(8, "Akkurat"),
+                grid = true,
+                gridalpha = 0.5,
+                gridstyle = :dash
+                )
+                u0[1] = u0[1] +0.1;
+end
+df = DataFrame(sol')
+df.t = sol.t
 
-savefig(fig,"C:\\Users\\liemann\\Desktop\\Präsi_DFG\\switched_stable.svg")
+
+savefig(fig2,"C:\\Users\\liemann\\Desktop\\Präsi_DFG\\example_hybrid_trajectory2.svg")
