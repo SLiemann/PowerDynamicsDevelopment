9011d09c844514b00e0ccb3b795878dd75d1062e
SebastianL <sebastian.liemann@tu-dortmund.de>
2023-01-11 17:10:39 +0100

    trying to implement new boundary determination

--- src/run/find_stable_region.jl
+++ src/run/find_stable_region.jl
@@ -52,7 +52,7 @@ plot([p1,p2])
 
 
 function CalcXRMap(Rrange, Xrange)
-    pg, ic =  Initialize_N32_GFM(1,1);
+    pg, ic =  Initialize_N32_GFM(1,0);
 
     length_dr = length(Rrange)
     length_dx = length(Xrange)
@@ -79,8 +79,8 @@ function CalcXRMap(Rrange, Xrange)
     return XR, XR_tend
 end
 
-Rverlauf = 20:-1:0.0
-Xverlauf = 20:-1:0.0
+Rverlauf = 20:-5:0.0
+Xverlauf = 10:-5:0.0
 
 @time xr, xrt = CalcXRMap(Rverlauf,Xverlauf);
 
@@ -107,11 +107,13 @@ function plotxkrit(vl_,xdata,ydata)
     x = Vector{Float64}()
     y = Vector{Float64}()
 
+    len1 = size(vl_)[1]
+    len1 = size(vl_)[2]
+    
     for i=1:size(vl_)[1]-1
         ind1 = findfirst(x->x==1,vl_[i,:])
         ind2 = findfirst(x->x==1,vl_[i+1,:])
-        #print(ind1)
-        #print(ind2)
+
         if i==1 
             push!(x,xdata[i])
             push!(y,ydata[ind1])
@@ -140,10 +142,99 @@ function plotxkrit(vl_,xdata,ydata)
     return x,y
 end
 
-px,py = plotxkrit(reverse(xr),collect(reverse(Rverlauf)),collect(reverse(Xverlauf)));
-plot(px,py)
+px1,py1 = plotxkrit(reverse(xr),collect(reverse(Rverlauf)),collect(reverse(Xverlauf)));
+plot(px1,py1)
 
-tmp = load("droop_R_110_1_0_X_70_1_0.jld")
+function DetermineBoundary(XR,Rv,Xv)
+    r = Vector{Float64}()
+    x = Vector{Float64}()
+    flag_dir = "bottom"
+    flag_end = false
+    ind = [(0,-1);(-1,0);(0,1);(1,0)]
+
+    Rv = reverse(Rv)
+    Xv = reverse(Xv)
+
+    lenx = size(XR)[1]
+    lenr = size(XR)[2]
+
+    #starting indices
+    indr = deepcopy(lenr)
+    indx = findlast(k->k==1,XR[indr,:])
+    #saving first entry
+    push!(r,Rv[indr])
+    push!(x,Xv[indx])
+
+    function next_ind(start_ind)
+        flag_end == false
+        for i=start_ind:start_ind+3
+            if mod(i,4) == 0
+                dr = ind[4][1]
+                dx = ind[4][2]
+            else
+                dr = ind[mod(i,4)][1]
+                dx = ind[mod(i,4)][2]
+            end
+
+            indr = indr + dr
+            indx = indx + dx
+
+            println(indr)
+            println(indx)
+            if indr == 0 || indr > lenr || indx == 0 || indx > lenx 
+                #skip boundarys
+            elseif indx == lenx 
+                println("END")
+                flag_end = true
+                break;
+            elseif XR[indr,indx] == -1
+                #skip unstable case
+            elseif XR[indr,indx] == 1
+                push!(r,Rv[indr])
+                push!(x,Xv[indx])
+                if mod(i,4) == 1
+                   flag_dir = "right"
+                   break;
+                elseif mod(i,4) == 2
+                   flag_dir = "bottom"
+                   break;
+                elseif mod(i,4) == 3
+                    flag_dir = "left"
+                    break;
+                elseif mod(i,4) == 0
+                    flag_dir = "top"
+                    break;
+                else
+                    error("error2")
+                end
+            end 
+        end
+        return nothing
+    end    
+    counter = 0;
+
+    while flag_end == false && counter < 10
+        println(flag_dir)
+        if flag_dir == "top"
+            next_ind(1)
+        elseif flag_dir == "left"
+            next_ind(2)
+        elseif flag_dir == "bottom"
+            next_ind(3)
+        elseif flag_dir == "right"
+            next_ind(4)
+        else
+            error("error")
+        end
+        counter = counter + 1
+    end
+    return r,x
+end
+
+r1,x1 = DetermineBoundary(xr,collect(Rverlauf),collect(Xverlauf));
+plot(r1,x1)
+
+tmp = load("droop_I070_R_18_1_0_X_08_1_0.jld")
 tmp = load("droop_I00_R_10_01_0_X_55_01_0.jld")
 tmp = load("matching_R_136_1_0_X_88_1_0.jld")
 
@@ -154,7 +245,7 @@ XRt= tmp["XR_t"];
 
 plot(surface(x=Rv,y=Xv,z=XRv))
 px,py = plotxkrit(reverse(XRv),collect(reverse(Rv)),collect(reverse(Xv)));
-plot(scatter(x=px,y=py))
+plot([scatter(x=px1,y=py1),scatter(x=px,y=py)])
 
 using MAT
 dir = "C:\\Users\\liemann\\Desktop\\"
