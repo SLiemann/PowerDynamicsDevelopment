103f9334008b75fdc574989cb9797501ac2404c4
SebastianL <sebastian.liemann@tu-dortmund.de>
2023-01-20 09:49:45 +0100

    add bus name in function name for uniqueness

--- src/operationpoint/GetCallbacks.jl
+++ src/operationpoint/GetCallbacks.jl
@@ -4,34 +4,34 @@ function GetCallbacks(pg::PowerGrid)
     cb = CallbackSet()
     for (ind,val) in enumerate(pg.nodes)
         if typeof(val[2]) == ThreePhaseFault
-            cb_tmp = Callback(val[2])
+            cb_tmp = Callback(val[2],val[1])
             cb = CallbackSet(cb,cb_tmp)
         end
     end
     return cb
 end
 
-function Callback(node::ThreePhaseFault)
+function Callback(node::ThreePhaseFault,nodename::String)
 
     t_on = 1.0
     t_off = 2.0
     
-    fun1 = "function timer_hit(u,t,integrator)
+    fun1 = "function timer_hit_$nodename(u,t,integrator)
                 t == $t_on
             end"
 
-    fun2 = "function timer_hit(u,t,integrator)
+    fun2 = "function timer_hit_$nodename(u,t,integrator)
                 t == $t_off
             end"
 
-    fun3 = "function Shortcircuiton(integrator)
+    fun3 = "function Shortcircuiton_$nodename(integrator)
                 display(1.0)
                 #integrator.p[$(node.p_ind[1])] = 80.0
                 #integrator.p[$(node.p_ind[2])] = 80.0
                 #initialize_dae!(integrator,BrownFullBasicInit())
             end"
 
-    fun4 = "function Shortcircuitoff(integrator)
+    fun4 = "function Shortcircuitoff_$nodename(integrator)
                 display(2.0)
                 #integrator.p[$(node.p_ind[1])] = 8e3
                 #integrator.p[$(node.p_ind[2])] = 8e3
